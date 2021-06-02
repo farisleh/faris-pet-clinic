5b0ffd5777652d0326fdfa16bac9951de6e3bdee
farisleh <farisleh1@gmail.com>
2021-06-03 00:13:21 +0800

    Initial Project Add

---
+++ src/main/java/faris/springframework/farispetclinic/FarisPetClinicApplication.java
@@ -0,0 +1,13 @@
+package faris.springframework.farispetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarisPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarisPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faris/springframework/farispetclinic/FarisPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package faris.springframework.farispetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarisPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
