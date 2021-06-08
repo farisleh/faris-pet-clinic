728e3ea0986b97f668a3f589ff83b8f5d35bd601
farisleh <farisleh1@gmail.com>
2021-06-08 16:02:22 +0800

    Implementation Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/faris/springframework/farispetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package faris.springframework.farispetclinic.services;
+
+import faris.springframework.farispetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/faris/springframework/farispetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package faris.springframework.farispetclinic.services;
+
+
+import faris.springframework.farispetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
